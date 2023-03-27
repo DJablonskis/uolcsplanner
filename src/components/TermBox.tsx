306b9cef80919f826d462fc538212aa6d8c5c3c4
Deividas Jablonskis <73912470+DJablonskis@users.noreply.github.com>
2023-03-27 21:00:47 +0100

    Update TermBox.tsx

--- src/components/TermBox.tsx
+++ src/components/TermBox.tsx
@@ -11,15 +11,17 @@ const TermBox = ({ terms, i, selection }: Props) => {
   let term = terms[i];
   return (
     <div className="bg-orange-100 mx-1 py-2 px-2">
-      <span className="ml-2">Term {i + 1}</span>
-      {term.takenCourses.length < 4 && (
-        <button
-          className="font-bold bg-sky-600 hover:bg-sky-400 hover:text-slate-700 text-slate-100 rounded mr-2 px-2 py-1  border-sky-900 ml-2"
-          onClick={() => selection(i)}
-        >
-          Add new course
-        </button>
-      )}
+      <div className="flex">
+        <span className="ml-2 mr-auto h4 ml-1">Term {i + 1}</span>
+        {term.takenCourses.length < 4 && (
+          <button
+            className="font-bold bg-sky-600 hover:bg-sky-400 hover:text-slate-700 text-slate-100 rounded px-2 py-1  border-sky-900 ml-2"
+            onClick={() => selection(i)}
+          >
+            + Add
+          </button>
+        )}
+      </div>
 
       <div>
         {term.takenCourses.map((tm) => (
@@ -30,10 +32,7 @@ const TermBox = ({ terms, i, selection }: Props) => {
             <p className="italic text-gray-700" style={{ maxWidth: "60ch" }}>
               No courses selected for this term.
             </p>
-            <p
-              className="italic text-gray-700 mt-2"
-              style={{ maxWidth: "60ch" }}
-            >
+            <p className="italic text-gray-700 mt-2">
               To add new courses - press "Add new course" button above.
             </p>
           </div>
